b2e2906e6f10d34e1b3c49b3763e1c2e5d665c50
Nicovel <nicovel98@gmail.com>
2024-07-02 06:43:19 -0500

    rutas creadas

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import { Image3 } from './components/Image3'
 import { Image4 } from './components/Image4'
 import { Image5 } from './components/Image5'
 import { Image6 } from './components/Image6'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <Image1 />
-      <Image2 />
-      <Image3 />
-      <Image4 />
-      <Image5 />
-      <Image6 />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/image1" element={<Image1 />} />
+          <Route path="/image2" element={<Image2 />} />
+          <Route path="/image3" element={<Image3 />} />
+          <Route path="/image4" element={<Image4 />} />
+          <Route path="/image5" element={<Image5 />} />
+          <Route path="/image6" element={<Image6 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
